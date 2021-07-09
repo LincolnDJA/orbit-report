fdcad77d0d6fb9bad617bc7eb28a166578a5b556
Dava Lincoln <lincolndja@gmail.com>
2021-07-09 16:15:42 -0500

    please

--- src/app/orbit-list/orbit-list.component.spec.ts
+++ src/app/orbit-list/orbit-list.component.spec.ts
@@ -5,20 +5,17 @@ import { OrbitListComponent } from './orbit-list.component';
 describe('OrbitListComponent', () => {
   let component: OrbitListComponent;
   let fixture: ComponentFixture<OrbitListComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ OrbitListComponent ]
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(OrbitListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
